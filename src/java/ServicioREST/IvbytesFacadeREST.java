259f761d04409fa89bb7a8828def48f877364773
Je¡ZZ¡ <JeZZ@DESKTOP-FVCO4FK>
2016-08-25 22:53:21 +0200

    REST SIN MODIFICAR

---
+++ src/java/ServicioREST/IvbytesFacadeREST.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServicioREST;
+
+import JPA_Entidades.Ivbytes;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Je¡ZZ¡
+ */
+@Stateless
+@Path("jpa_entidades.ivbytes")
+public class IvbytesFacadeREST extends AbstractFacade<Ivbytes> {
+
+    @PersistenceContext(unitName = "SiguemePU")
+    private EntityManager em;
+
+    public IvbytesFacadeREST() {
+        super(Ivbytes.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Ivbytes entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") String id, Ivbytes entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Ivbytes find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Ivbytes> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Ivbytes> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
---
+++ src/java/ServicioREST/PosicionFacadeREST.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServicioREST;
+
+import JPA_Entidades.Posicion;
+import JPA_Entidades.PosicionPK;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.PathSegment;
+
+/**
+ *
+ * @author Je¡ZZ¡
+ */
+@Stateless
+@Path("jpa_entidades.posicion")
+public class PosicionFacadeREST extends AbstractFacade<Posicion> {
+
+    @PersistenceContext(unitName = "SiguemePU")
+    private EntityManager em;
+
+    private PosicionPK getPrimaryKey(PathSegment pathSegment) {
+        /*
+         * pathSemgent represents a URI path segment and any associated matrix parameters.
+         * URI path part is supposed to be in form of 'somePath;pruebaId=pruebaIdValue;usuarioId=usuarioIdValue;fecha=fechaValue;hora=horaValue'.
+         * Here 'somePath' is a result of getPath() method invocation and
+         * it is ignored in the following code.
+         * Matrix parameters are used as field names to build a primary key instance.
+         */
+        JPA_Entidades.PosicionPK key = new JPA_Entidades.PosicionPK();
+        javax.ws.rs.core.MultivaluedMap<String, String> map = pathSegment.getMatrixParameters();
+        java.util.List<String> pruebaId = map.get("pruebaId");
+        if (pruebaId != null && !pruebaId.isEmpty()) {
+            key.setPruebaId(pruebaId.get(0));
+        }
+        java.util.List<String> usuarioId = map.get("usuarioId");
+        if (usuarioId != null && !usuarioId.isEmpty()) {
+            key.setUsuarioId(usuarioId.get(0));
+        }
+        java.util.List<String> fecha = map.get("fecha");
+        if (fecha != null && !fecha.isEmpty()) {
+            key.setFecha(new java.util.Date(fecha.get(0)));
+        }
+        java.util.List<String> hora = map.get("hora");
+        if (hora != null && !hora.isEmpty()) {
+            key.setHora(new java.util.Date(hora.get(0)));
+        }
+        return key;
+    }
+
+    public PosicionFacadeREST() {
+        super(Posicion.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Posicion entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") PathSegment id, Posicion entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") PathSegment id) {
+        JPA_Entidades.PosicionPK key = getPrimaryKey(id);
+        super.remove(super.find(key));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Posicion find(@PathParam("id") PathSegment id) {
+        JPA_Entidades.PosicionPK key = getPrimaryKey(id);
+        return super.find(key);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Posicion> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Posicion> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
