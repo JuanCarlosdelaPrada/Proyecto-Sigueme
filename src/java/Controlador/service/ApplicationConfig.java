f27366dc8826045addd6dd81101e2c3f0fc7999c
Je¡ZZ¡ <JeZZ@DESKTOP-FVCO4FK>
2016-08-25 15:26:23 +0200

    modificar nombres

---
+++ src/java/Controlador/service/ApplicationConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Je¡ZZ¡
+ */@javax.ws.rs.ApplicationPath("api")
+public class ApplicationConfig extends Application{
+    
+     @Override
+     public Set<Class<?>> getClasses() {
+         Set<Class<?>> resources = new java.util.HashSet<>();
+         addRestResourceClasses(resources);
+         return resources;
+     }
+     
+     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(Controlador.service.InscritoFacadeREST.class);
+        resources.add(Controlador.service.PosicionFacadeREST.class);
+        resources.add(Controlador.service.PruebaFacadeREST.class);
+        resources.add(Controlador.service.RutaFacadeREST.class);
+        resources.add(Controlador.service.UsuarioFacadeREST.class);
+     }
+}
