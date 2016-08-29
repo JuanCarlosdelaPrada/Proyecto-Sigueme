a5bf598c7e79e1b034d1bc25dd1a5bb539c3f17e
Je¡ZZ¡ <JeZZ@DESKTOP-FVCO4FK>
2016-08-29 20:24:14 +0200

    REST FUNCIONANDO

--- src/java/ServicioREST/ApplicationConfig.java
+++ src/java/ServicioREST/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
  *
  * @author Je¡ZZ¡
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@javax.ws.rs.ApplicationPath("servicio-rest")
 public class ApplicationConfig extends Application {
 
     @Override
